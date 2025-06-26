c11cf1b85f470610672c25cfec05f5fb641cd485
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2025-06-26 07:39:54 +0530

    react

--- firstapp/src/App.jsx
+++ firstapp/src/App.jsx
@@ -1,46 +1,58 @@
-import reactImg from "./assets/download.png";
-import react2Img from "./assets/react-query.2d296c3a.png";
-import './App.css';
+import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
 
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
-const reactDescription = ['Fundamental ', "crucial", "essential", "important", "necessary", "vital", "indispensable", "key", "critical", "pivotal", "integral", "paramount", "significant", "crucial"];
-
-
-function getRandomDescription(max) { 
-    return Math.floor(Math.random() * (max + 1));
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
 }
 
-
-
-
 function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
 
-  const description = reactDescription[getRandomDescription(13)];
-
-  
   return (
-    <header className="App-header">
-      <img src={react2Img} alt="" />
-      <h1> React Essentials</h1>
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
       <p>
-        {description} React Concepts You Will Need For Almost Any app You are
-        Goin To Build.
+        {description} React concepts you will need for almost any app you are
+        going to build!
       </p>
     </header>
   );
 }
 
-
-function App() { 
+function CoreConcept(props) {
   return (
-    <div className="App">
-      <Header />
-      <main>
-        <h2>Time to get START</h2>
-        
-      </main>
-    </div>
-  )
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="The core UI building block."
+              image={componentsImg}
+            />
+            <CoreConcept title="Props" />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
